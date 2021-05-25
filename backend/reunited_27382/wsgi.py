20846b681e26abb22e0d8296f352264ea9927a76
Crowdbotics <team@crowdbotics.com>
2021-05-25 16:53:07 +0000

    Update models

--- backend/reunited_27382/wsgi.py
+++ backend/reunited_27382/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reunited_27382.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reunited_27382.settings")
 
 application = get_wsgi_application()
